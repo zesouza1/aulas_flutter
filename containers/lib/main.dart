3aa799686491098af6a6a72e98fd24764bf756a5
zesouza1 <wellington.sojo@protonmail.com>
2024-04-19 21:38:16 -0300

    Pronto

--- containers/lib/main.dart
+++ containers/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -20,135 +19,165 @@ class MyApp extends StatelessWidget {
             children: [
               Row(
                   children: [
-                  Container(
-                    padding: EdgeInsets.all(50.0),
-                    height: 150,
-                    width: 250,
-                    color: Colors.red,
-                    child: const Text(
-                      'Iphone',
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 150,
+                      width: 250,                      
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        boxShadow: [
+                          BoxShadow(
+                            color:Colors.grey,
+                            spreadRadius:1,
+                            blurRadius:200,
+                            offset:Offset(0,2),
+                        )],
+                      ),
+                      child: const Text(
+                        'Iphone',
+                          style: TextStyle(
+                          fontSize: 50,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),                  
+                   Expanded(
+                     child: Column(
+                      children: [
+                        Center(child: Text('Teste 1')),
+                        Divider(),
+                        Text('Teste 2'),
+                        Divider(),
+                        Text('Teste 3'),
+                      ],
+                                       ),
+                   ),
+                ],
+              ),
+              Divider(),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Container(
+                      height: 150,
+                      width: 250,                      
+                      decoration: const BoxDecoration(
+                        color: Colors.green,
+                        boxShadow: [
+                          BoxShadow(
+                            color:Colors.grey,
+                            spreadRadius:1,
+                            blurRadius:200,
+                            offset:Offset(0,2),
+                        )],
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Android',
+                          style: TextStyle(
+                            fontSize: 50,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                    child: Column(                   
+                      children: [
+                        Text('Teste 1',style: TextStyle(fontWeight:FontWeight.bold)),
+                        Divider(),
+                        Text('Teste 2'),
+                        Divider(),
+                        Text('Teste 3'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Divider(),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 150,
+                      width: 250,                      
+                      decoration: const BoxDecoration(
+                        color: Colors.pink,
+                        boxShadow: [
+                          BoxShadow(
+                            color:Colors.grey,
+                            spreadRadius:1,
+                            blurRadius:200,
+                            offset:Offset(0,2),
+                        )],
+                      ),
+                      child: const Text(
+                        'Celular',
                         style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
+                          fontSize: 50,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                  Column(
-                    children: [
-                      Text('Teste 1'),
-                      Divider(),
-                      Text('Teste 2'),
-                      Divider(),
-                      Text('Teste 3'),
-                    ],
+                  const Expanded(
+                    child: Column(
+                      children: [
+                        Text('Teste 1'),
+                        Divider(),
+                        Text('Teste 2'),
+                        Divider(),
+                        Text('Teste 3'),
+                      ],
+                    ),
                   ),
                 ],
               ),
               Divider(),
               Row(
                 children: [
-                  Container(
-                    padding: EdgeInsets.all(50.0),
-                    height: 150,
-                    width: 250,
-                    color: Colors.green,
-                    child: const Text(
-                      'Android',
-                      style: TextStyle(
-                        fontSize: 20,
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 150,
+                      width: 250,                      
+                      decoration: const BoxDecoration(
                         color: Colors.black,
+                        boxShadow: [
+                          BoxShadow(
+                            color:Colors.grey,
+                            spreadRadius:1,
+                            blurRadius:200,
+                            offset:Offset(0,2),
+                        )],
+                      ),
+                      child: const Text(
+                        'Carregador',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                  Column(
-                    children: [
-                      Text('Teste 1'),
-                      Divider(),
-                      Text('Teste 2'),
-                      Divider(),
-                      Text('Teste 3'),
-                    ],
-                  ),
-                ],
-              ),
-              Divider(),
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(50.0),
-                    height: 150,
-                    width: 250,
-                    color: Colors.pink,
-                    child: const Text(
-                      'Celular',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
+                  const Expanded(
+                    child: Column(
+                      children: [
+                        Text('Teste 1'),
+                        Divider(),
+                        Text('Teste 2'),
+                        Divider(),
+                        Text('Teste 3'),
+                      ],
                     ),
-                  ),
-                  Column(
-                    children: [
-                      Text('Teste 1'),
-                      Divider(),
-                      Text('Teste 2'),
-                      Divider(),
-                      Text('Teste 3'),
-                    ],
-                  ),
-                ],
-              ),
-              Divider(),
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(50.0),
-                    height: 150,
-                    width: 250,
-                    color: Colors.black,
-                    child: const Text(
-                      'Carregador',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Text('Teste 1'),
-                      Divider(),
-                      Text('Teste 2'),
-                      Divider(),
-                      Text('Teste 3'),
-                    ],
-                  ),
-                ],
-              ),
-              Divider(),
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(50.0),
-                    height: 150,
-                    width: 250,
-                    color: Color.fromARGB(255, 39, 1, 255),
-                    child: const Text(
-                      'Carregador',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Text('Teste 1'),
-                      Divider(),
-                      Text('Teste 2'),
-                      Divider(),
-                      Text('Teste 3'),
-                    ],
                   ),
                 ],
               ),
